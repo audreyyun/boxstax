3297cabf29e680d3ec3beb5d2664395f5ded3180
audreyyun <96audreyyun@gmail.com>
2021-02-09 11:56:24 -0500

    Make box move laterally and bounce off walls

--- src/index.js
+++ src/index.js
@@ -1,78 +1,6 @@
 import "./styles/index.scss";
 
-// document.addEventListener('DOMContentLoaded', function () {
-
-//   const canvas = document.getElementById('canvas');
-//   const ctx = canvas.getContext('2d');
-
-
-//   ctx.beginPath();
-//   ctx.rect(20, 40, 50, 50);
-//   ctx.fillStyle = "#FF0000";
-//   ctx.fill();
-//   ctx.closePath();
-
-//   ctx.beginPath();
-//   ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-//   ctx.fillStyle = "green";
-//   ctx.fill();
-//   ctx.closePath();
-
-//   ctx.beginPath();
-//   ctx.rect(160, 10, 100, 40);
-//   ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-//   ctx.stroke();
-//   ctx.closePath();
-//   // ctx.fillStyle = 'green';
-//   // ctx.fillRect(10, 10, 150, 100);
-//   window.ctx = ctx; //just so we have access so we can test
-
-// });
-
-
-
-// import "./images/yoda-stitch.jpg";
-// import canvasExample from "./scripts/canvas";
-//   // import Square from "./scripts/square";
-//   // import { DOMExample } from "./scripts/DOMExample";
-// // const currentStateObj = {
-// //   currentExample: null,
-// //   currentEventListeners: [],
-// // };
-
-
-// document.querySelector("#canvas-demo").addEventListener("click", startCanvas);
-// document.querySelector("#DOM-demo").addEventListener("click", startDOM);
-
-
 document.addEventListener('DOMContentLoaded', () => {
-
-  // const canvas = document.getElementById('myCanvas');
-  // const ctx = canvas.getContext('2d');
-  // let x = canvas.width / 2;
-  // let y = canvas.height - 30;
-  // let dx = 2;
-  // let dy = -2;
-
-
-
-  // function draw() { 
-  //   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  //   drawBox();
-  //   x += dx;
-  //   y += dy;
-  // }
-  
-
-  // function drawBox() { 
-  //   ctx.beginPath();
-  //   ctx.rect(20, 20, 150, 100);
-  //   // ctx.fillStyle = "#0095DD";
-  //   ctx.fill();
-  //   ctx.stroke();
-  // }
-  
-  // let interval = setInterval(draw, 10);
 
   const canvas = document.getElementById("myCanvas");
   const ctx = canvas.getContext("2d");
@@ -99,7 +27,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
   }
-  //click just make it box move faster left and right
+
+  
   canvas.addEventListener('click', (e) => {
     requestAnimationFrame(drawBox);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
